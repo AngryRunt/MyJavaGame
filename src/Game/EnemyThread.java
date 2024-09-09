bd7e56077ddd0b949dfaf592264b220dccafa626
Natalia Tabunkina <nataliatabunkina@gmail.com>
2024-09-09 18:29:39 +0300

    Move game to game dir

--- src/Game/EnemyThread.java
+++ src/Game/EnemyThread.java
@@ -1,3 +1,5 @@
+package Game;
+
 public class EnemyThread extends Thread {
     @Override
     public void run() {
--- src/Game/GameScene.java
+++ src/Game/GameScene.java
@@ -1,3 +1,5 @@
+package Game;
+
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
